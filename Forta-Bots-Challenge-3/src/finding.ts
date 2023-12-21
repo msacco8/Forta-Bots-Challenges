1747b50ef24c1bb6a7bce9b9f8a85321fd0a48ea
msacco8 <sacco815@gmail.com>
2023-12-21 18:08:28 -0500

    fix: fixed wording in create finding

--- Forta-Bots-Challenge-3/src/finding.ts
+++ Forta-Bots-Challenge-3/src/finding.ts
@@ -4,7 +4,7 @@ import { BigNumber } from "ethers";
 export const createFinding = (chainId: number, chainName: string, supplies: BigNumber[]) => {
   return Finding.fromObject({
     name: "L2 Dai Supply Invariant Violated",
-    description: `The supply of ${chainName} DAI is less than the DAI held in the L1 escrow contract.`,
+    description: `The DAI held in the L1 escrow contract is less than the supply of ${chainName} DAI.`,
     alertId: "NETHERMIND-1",
     severity: FindingSeverity.Low,
     type: FindingType.Info,
